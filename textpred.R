7ad88ab346e3e9d13f2926a0dead13ffd8f0de11
Keith <64439350+kpost34@users.noreply.github.com>
2020-09-26 15:14:54 -0600

    Update textpred.R

--- textpred.R
+++ textpred.R
@@ -1,52 +1,48 @@
 textpred<-function(text){
-        require("quanteda")
-        require("stringr")
-        require("dplyr")
-        text<-gsub("[[:punct:]]+","",text)
-        text<-trimws(gsub(pattern="\\s+"," ",text))
-        text<-tolower(text)
-        if(ntoken(text)>4) {text<-word(text,-4,-1)}
-        if(ntoken(text)==4){
-            t3_5grams<-dt_5grams_freq %>% 
-                filter(pre_5grams==text) %>%
-                slice_max(order_by=freq,n=3,with_ties=FALSE) 
-            if(nrow(t3_5grams)==0){
-                text<-word(text,-3,-1)
-            }
-            else{
-                return(noquote(t3_5grams$last_5grams))
-            }
-        }
-        if(ntoken(text)==3){
-            t3_4grams<-dt_4grams_freq %>% 
-                filter(pre_4grams==text) %>%
-                slice_max(order_by=freq,n=3,with_ties=FALSE) 
-            if(nrow(t3_4grams)==0){
-                text<-word(text,-2,-1)
-            }
-            else{
-                return(noquote(t3_4grams$last_4grams))
-            }
-        }
-        if(ntoken(text)==2){
-            t3_trigrams<-dt_trigrams_freq %>% 
-                filter(pre_trigrams==text) %>%
-                slice_max(order_by=freq,n=3,with_ties=FALSE) 
-            if(nrow(t3_trigrams)==0){
-                text<-word(text,-1,-1)
-            }
-            else{
-                return(noquote(t3_trigrams$last_trigrams))
-            }
-        }
-        if(ntoken(text)==1){
-            t3_bigrams<-dt_bigrams_freq %>% 
-                filter(pre_bigrams==text) %>%
-                slice_max(order_by=freq,n=3,with_ties=FALSE) 
-            if(nrow(t3_bigrams)==0){
-                return(print(noquote("<Unk>")))
-            }
-            else{return(noquote(t3_bigrams$last_bigrams))
-            }
-        }
-    }
+	require("quanteda")
+	require("stringr")
+	require("dplyr")
+	text<-gsub("[[:punct:]]+","",text)
+	text<-trimws(gsub(pattern="\\s+"," ",text))
+	text<-tolower(text)
+	if(ntoken(text)>4) {text<-word(text,-4,-1)}
+	if(ntoken(text)==4){
+		t3_5grams<-dt_5grams_freq %>% 
+			filter(pre_5grams==text) 
+		if(nrow(t3_5grams)==0){
+			text<-word(text,-3,-1)
+		}
+		else{
+			return(noquote(t3_5grams$last_5grams))
+		}
+	}
+	if(ntoken(text)==3){
+		t3_4grams<-dt_4grams_freq %>% 
+			filter(pre_4grams==text) 
+		if(nrow(t3_4grams)==0){
+			text<-word(text,-2,-1)
+		}
+		else{
+			return(noquote(t3_4grams$last_4grams))
+		}
+	}
+	if(ntoken(text)==2){
+		t3_trigrams<-dt_trigrams_freq %>% 
+			filter(pre_trigrams==text) 
+		if(nrow(t3_trigrams)==0){
+			text<-word(text,-1,-1)
+		}
+		else{
+			return(noquote(t3_trigrams$last_trigrams))
+		}
+	}
+	if(ntoken(text)==1){
+		t3_bigrams<-dt_bigrams_freq %>% 
+			filter(pre_bigrams==text) 
+		if(nrow(t3_bigrams)==0){
+			return(print(noquote("<Unk>")))
+		}
+		else{return(noquote(t3_bigrams$last_bigrams))
+		}
+	}
+}
